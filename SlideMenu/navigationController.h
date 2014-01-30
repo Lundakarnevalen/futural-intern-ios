e022222710212f77ee4c9889057aeefba7dcf766
antonschleenvoigt <anton.schleenvoigt@gmail.com>
2014-01-30 21:45:23 +0100

    Changed color to white

---
+++ SlideMenu/navigationController.h
@@ -0,0 +1,13 @@
+//
+//  navigationController.h
+//  Karnevalist2014
+//
+//  Created by contributeshopping on 2014-01-30.
+//  Copyright (c) 2014 Indee Box LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface navigationController : UINavigationController
+
+@end
