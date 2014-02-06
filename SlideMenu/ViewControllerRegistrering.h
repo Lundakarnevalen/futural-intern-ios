70a04ff0dfa8f36dbcc5763d458a5b34fb29b70b
Richard Luong <richardluong92@gmail.com>
2014-02-06 17:06:26 +0100

    implemented #6

--- SlideMenu/ViewControllerRegistrering.h
+++ SlideMenu/ViewControllerRegistrering.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewControllerWebView : UIViewController
+@interface ViewControllerRegistrering : UIViewController <UIWebViewDelegate>
 
 {
     IBOutlet UIWebView *myWebView;
--- SlideMenu/ViewControllerRegistrering.m
+++ SlideMenu/ViewControllerRegistrering.m
@@ -6,15 +6,17 @@
 //  Copyright (c) 2014 Indee Box LLC. All rights reserved.
 //
 
-#import "ViewControllerWebView.h"
+#import "ViewControllerRegistrering.h"
 #import "ECSlidingViewController.h"
 #import "MenuViewController.h"
 
-@interface ViewControllerWebView ()
+@interface ViewControllerRegistrering ()
+
+@property UIActivityIndicatorView *spinner;
 
 @end
 
-@implementation ViewControllerWebView
+@implementation ViewControllerRegistrering
 
 - (IBAction)revealMenu:(id)sender
 {
@@ -33,13 +35,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
     
-    NSURL *myWeview = [NSURL URLWithString:@"http://www.karnevalist.se/karnevalister/step1"];
+    //spinner
+    self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.spinner.center = self.view.center;
+    self.spinner.frame = CGRectMake(self.spinner.frame.origin.x-10, self.spinner.frame.origin.y-10, self.spinner.frame.size.width+20, self.spinner.frame.size.height+20);
+    [self.spinner.layer setCornerRadius:3.0f];
+    [self.spinner setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.2]];
+    [self.view addSubview:self.spinner];
     
-    NSURLRequest *myrequest = [NSURLRequest requestWithURL:myWeview];
-    
+    NSURL *webURL = [NSURL URLWithString:@"http://www.karnevalist.se/karnevalister/step1"];
+    NSURLRequest *myrequest = [NSURLRequest requestWithURL:webURL];
     [myWebView loadRequest:myrequest];
+    myWebView.delegate = self;
 
     //Swipe höger
 	UISwipeGestureRecognizer  *swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handlerightSwipe:)];
@@ -48,19 +56,31 @@
     swipeRight.direction = UISwipeGestureRecognizerDirectionRight;
     [self.view addGestureRecognizer:swipeRight];
     
-    // Do any additional setup after loading the view.
-    
     self.view.layer.shadowOpacity = 0.75f;
     self.view.layer.shadowRadius = 10.0f;
     self.view.layer.shadowColor = [UIColor blackColor].CGColor;
     
-    
     if (![self.slidingViewController.underLeftViewController isKindOfClass:[MenuViewController class]]) {
         self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
     }
-    
     [self.view addGestureRecognizer:self.slidingViewController.panGesture];
     
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    UIApplication *application = [UIApplication sharedApplication];
+    application.networkActivityIndicatorVisible = YES;
+    [self.spinner startAnimating];
+    
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    UIApplication *application = [UIApplication sharedApplication];
+    application.networkActivityIndicatorVisible = NO;
+    
+    self.spinner.hidden = YES;
+    [self.spinner stopAnimating];
+    [self.spinner removeFromSuperview];
 }
 
 - (void)didReceiveMemoryWarning
